b1a7eacb0155674ed2c0ee32a8140cc85423779e
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-04-30 13:03:23 -0700

    Selection sort function

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -18,10 +18,12 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[m_num_idx]) m_num_idx = j;
 		
 		if (m_num_idx !=  i)
-                {
-                      temp = array[i];
-                      array[i] = array[m_num_idx];
-                      array[m_num_idx] = temp;
+		{
+		temp = array[i];
+		array[i] = array[m_num_idx];
+		array[m_num_idx] = temp;
+
+		printf("Swap process %d and %d: ", array[i], array[m_num_idx]);
                 }
 		print_array(array, size);
 
