30c78c10b593036221341c600b234c3238119a2f
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-04-30 10:24:48 -0700

    The function and time complexity of the selection sorting algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "sort.h"
+/**
+ * selection_sort - The selection sorting method
+ *
+ * @array: This is the array that contain the values
+ * @size: This is the size of the array
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, temp, j, m_num_idx;
+
+	for (i = 0; i < size; i++)
+	{
+		m_num_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[m_num_idx]) m_num_idx = j;
+		
+		if (m_num_idx !=  i)
+                {
+                      temp = array[i];
+                      array[i] = array[m_num_idx];
+                      array[m_num_idx] = temp;
+                }
+		print_array(array, size);
+
+		
+
+	}
+
+
+
+}
